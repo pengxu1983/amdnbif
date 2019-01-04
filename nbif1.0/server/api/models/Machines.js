cae2bed78d8842048d093110ea0c55256dc482e9
Peng <benpeng@srdcws806.amd.com>
2019-01-04 18:08:05 +0800

    dbg

--- nbif1.0/server/api/models/Machines.js
+++ nbif1.0/server/api/models/Machines.js
@@ -33,6 +33,9 @@ module.exports = {
     testStatus    : {
       type        : 'string',
       columnType  : 'longtext'
+    },
+    roll          : {
+      type        : 'string'
     }
     //  ╔═╗╔╦╗╔╗ ╔═╗╔╦╗╔═╗
     //  ║╣ ║║║╠╩╗║╣  ║║╚═╗
