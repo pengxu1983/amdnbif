97757b235c0053fbefa0d1c1ad93f9061a579592
pengxu1983 <pengxu1983@163.com>
2019-01-28 14:53:05 +0800

    dbg

--- nbif1.0/web/main/src/components/SanityPage_sub/SanityPage_config_main.vue
+++ nbif1.0/web/main/src/components/SanityPage_sub/SanityPage_config_main.vue
@@ -4,7 +4,6 @@
         <el-form :inline="true" :model="projectinfo" class="demo-form-inline">
           <el-form-item label="ProjectName">
             <el-select v-model="projectinfo.projectname" placeholder="ProjectName"
-              @change="get()"
             >
               <el-option 
                 v-for="oneproject in projects" 
@@ -16,7 +15,6 @@
           </el-form-item>
           <el-form-item label="VariantName">
             <el-select v-model="projectinfo.variantname" placeholder="VariantName"
-              @change="get()"
             >
               <el-option 
                 v-for="onevariant in variants" 
