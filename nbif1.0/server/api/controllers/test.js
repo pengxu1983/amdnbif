c2d085bb673f3f8361c0c20ea2ada248165f0b1c
pengxu1983 <pengxu1983@163.com>
2019-02-07 22:04:10 +0800

    dbg

---
+++ nbif1.0/server/api/controllers/test.js
@@ -0,0 +1,30 @@
+module.exports = {
+
+
+  friendlyName: 'Test',
+
+
+  description: 'Test something.',
+
+
+  inputs: {
+    kind  : {
+      type  : 'string'
+    }
+  },
+
+
+  exits: {
+
+  },
+
+
+  fn: async function (inputs) {
+    
+    // All done.
+    return;
+
+  }
+
+
+};
