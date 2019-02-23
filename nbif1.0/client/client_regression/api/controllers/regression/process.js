51233152112bd92c3469ba4f6c1fdb37e90ddcd1
pengxu1983 <pengxu1983@163.com>
2019-02-23 16:21:41 +0800

    dbg

--- nbif1.0/client/client_regression/api/controllers/regression/process.js
+++ nbif1.0/client/client_regression/api/controllers/regression/process.js
@@ -12,96 +12,66 @@ let variantname = 'nbif_al_gpu';
 let mode  = 'normal';
 let loop  = 'daily';
 let time  = moment().format('YYYYMMDDHHmmss');
+let kickoffdate  = moment().format('YYYY-MM-DD');
 let currentCL ;
-var jobid_regression_main_daily_check_status = new cronJob('0 0 * * * *',function(){
+var jobid_regression_main_daily_check_status = new cronJob('0 30 * * * *',function(){
   console.log('jobid_regression_main_daily start at '+moment().format('YYYY-MM-DD HH:mm:ss'));
   let treeRoot = workspace+'/nbif.regression.main.daily';
-  let outDir   = treeRoot+'/out/linux_2.6.32_64.VCS/nbif_al_gpu/config/nbif_all_rtl/run/nbif-al_gpu-mero/nbiftdl';
+  let outDir  = {};
+  let availableSuite = ['nbiftdl','nbifresize','nbifrandom','nbifgen4','nbifdummyf'];
+  outDir['nbiftdl'] = treeRoot+'/out/linux_2.6.32_64.VCS/nbif_al_gpu/config/nbif_all_rtl/run/nbif-al_gpu-mero/nbiftdl';
+  outDir['nbifresize'] = treeRoot+'/out/linux_2.6.32_64.VCS/nbif_al_gpu/config/nbif_all_rtl/run/nbif-al_gpu-mero/nbifresize';
+  outDir['nbifrandom'] = treeRoot+'/out/linux_2.6.32_64.VCS/nbif_al_gpu/config/nbif_all_rtl/run/nbif-al_gpu-mero/nbifrandom';
+  outDir['nbifgen4'] = treeRoot+'/out/linux_2.6.32_64.VCS/nbif_al_gpu/config/nbif_all_rtl/run/nbif-al_gpu-mero/nbifgen4';
+  outDir['nbifdummyf'] = treeRoot+'/out/linux_2.6.32_64.VCS/nbif_al_gpu/config/nbif_all_rtl/run/nbif-al_gpu-mero/nbifdummyf';
   let testList = [];
   let testResult = {};
   //get test list
   if(fs.existsSync(treeRoot+'/testlist.log')){
-    fs.readFile(treeRoot+'/testlist.log','utf8',(err,data)=>{
-      if(err){
-        console.log(err);
+    let lines = fs.readFileSync(treeRoot+'/testlist.log','utf8').split('\n');
+    lines.pop();
+    let regx = /evaluation of 'testcase/;
+    for(let l=0;l<lines.length;l++){
+      if(regx.test(lines[l])){
+        let R = lines[l].split('/');
+        let RR = R[1].split('_nbif_all_rtl');
+        let RRR = R[0].split('::');
+        testList.push(RR[0]);
+        testResult[RR[0]]={};
+        testResult[RR[0]]['suite']  = RRR[1];
       }
-      else{
-        let lines = data.split('\n');
-        lines.pop();
-        let regx = /evaluation of 'testcase/;
-        for(let l=0;l<lines.length;l++){
-          if(regx.test(lines[l])){
-            let R = lines[l].split('/');
-            let RR = R[1].split('_nbif_all_rtl');
-            testList.push(RR[0]);
-            testResult[RR[0]]={};
-          }
-        }
-      }
-    });
+    }
   }
   else {
     return;
   }
   //check status per test
-  let regx0 = /\+seed=(\d+)/;
-  let regx1 = /^Error/i;
-  let regx2 = /^UVM_ERROR/;
-  let regx3 = /^UVM_ERROR\s+:\s+\d+/;
-  let regx4 = /^UVM_FATAL/;
-  let regx5 = /^UVM_FATAL\s+:\s+\d+/;
   for(let t=0;t<testList.length;t++){
+    console.log('T :'+t);
+    console.log(testList[t]);
+    testResult[testList[t]]['kickoffdate']= kickoffdate;
     testResult[testList[t]][projectname]  = projectname;
     testResult[testList[t]][variantname]  = variantname;
     testResult[testList[t]]['changelist'] = currentCL;
     testResult[testList[t]]['result']     = 'UNKNOWN';
     testResult[testList[t]]['seed']       = 'NA';
     testResult[testList[t]]['signature']  = 'NA';
-    if(fs.existsSync(outDir+'/'+testList[t]+'_nbif_all_rtl/REGRESS_PASS')){
+    if(fs.existsSync(outDir['nbiftdl']+'/'+testList[t]+'_nbif_all_rtl/REGRESS_PASS')){
       testResult[testList[t]]['result']     = 'PASS';
       testResult[testList[t]]['seed']       = 'NA';
       testResult[testList[t]]['signature']  = 'NA';
+      console.log(testList[t]+' is PASS');
+      console.log(testResult[testList[t]]);
     }
-    else if(fs.existsSync(outDir+'/'+testList[t]+'_nbif_all_rtl/vcs_run.log')){
-      fs.readFile(outDir+'/'+testList[t]+'_nbif_all_rtl/vcs_run.log','utf8',(err,data)=>{
-        if(err){
-          console.log(err);
-        }
-        else{
-          let lines = data.split('\n');
-          lines.pop();
-          for(let l =0;l<lines.length;l++){
-            if(regx0.test(lines[l])){
-              lines[l].replace(regx0,function($0,$1){
-                testResult[testList[t]]['seed']       = $1;
-              });
-            }
-            if(regx1.test(lines[l])){
-              testResult[testList[t]]['result']     = 'FAIL';
-              testResult[testList[t]]['signature']  = lines[l];
-              break;
-            }
-            if(regx2.test(lines[l])){
-              if(regx3.test(lines[l])){
-                //ignore
-                break;
-              }
-              testResult[testList[t]]['result']     = 'FAIL';
-              testResult[testList[t]]['signature']  = lines[l];
-              break;
-            }
-            if(regx4.test(lines[l])){
-              if(regx5.test(lines[l])){
-                //ignore
-                break;
-              }
-              testResult[testList[t]]['result']     = 'FAIL';
-              testResult[testList[t]]['signature']  = lines[l];
-              break;
-            }
-          }
-        }
+    else if(fs.existsSync(outDir['nbiftdl']+'/'+testList[t]+'_nbif_all_rtl/vcs_run.log')){
+      let R =child_process.execSync(workspace+'/amdnbif/nbif1.0/client/client_regression/tools/processSimLog.pl '+outDir['nbiftdl']+'/'+testList[t]+'_nbif_all_rtl/vcs_run.log',{
+        encoding  : 'utf8',
+        maxBuffer : 1024*1024*100
       });
+      console.log(R.split('\n'));
+      testResult[testList[t]]['seed']       = R[0];
+      testResult[testList[t]]['result']     = R[1];
+      testResult[testList[t]]['signature']  = R[2];
     }
     else{
       //unknown status
@@ -109,9 +79,10 @@ var jobid_regression_main_daily_check_status = new cronJob('0 0 * * * *',functio
   }
   //send result per test
 },null,false,'Asia/Chongqing');
-var jobid_regression_main_daily = new cronJob('0 23 13 * * *',function(){
+var jobid_regression_main_daily = new cronJob('0 30 16 * * *',function(){
   console.log('jobid_regression_main_daily start at '+moment().format('YYYY-MM-DD HH:mm:ss'));
-  
+  jobid_regression_main_daily_check_status.stop();
+  console.log('jobid_regression_main_daily_check_status stopped due to new kickoff at '+moment().format('YYYY-MM-DD HH:mm:ss'));
   //find info from DB
   let postData = querystring.stringify({
     'kind': 'regressioninfo'
@@ -150,6 +121,18 @@ var jobid_regression_main_daily = new cronJob('0 23 13 * * *',function(){
           }
           else{
           }
+          if(fs.existsSync(treeRoot+'/build.log')){
+            child_process.execSync('mv '+treeRoot+'/build.log'+' '+treeRoot+'/build.log.toRemove');
+            child_process.exec('rm -rf '+treeRoot+'/build.log.toRemove');
+          }
+          if(fs.existsSync(treeRoot+'/run.log')){
+            child_process.execSync('mv '+treeRoot+'/run.log'+' '+treeRoot+'/run.log.toRemove');
+            child_process.exec('rm -rf '+treeRoot+'/run.log.toRemove');
+          }
+          if(fs.existsSync(treeRoot+'/testlist.log')){
+            child_process.execSync('mv '+treeRoot+'/testlist.log'+' '+treeRoot+'/testlist.log.toRemove');
+            child_process.exec('rm -rf '+treeRoot+'/testlist.log.toRemove');
+          }
         }
         //prepare the script
         text += '#!/tool/pandora64/bin/tcsh\n';
@@ -170,7 +153,7 @@ var jobid_regression_main_daily = new cronJob('0 23 13 * * *',function(){
         text += 'source useful_cmd -cyb -proj '+projectname+'\n';
         text += 'set batch_name_v = `/tool/pandora64/.package/perl-5.24.0/bin/perl '+treeRoot+'/src/test/tools/scripts/get_latest_batch_name.pl -r -mode -p '+variantname+' -c nbif_all_rtl -m normal`\n';
         text += 'regrsys_prep_wa -no-chmod\n';
-        text += 'dj -l testlist.log -DDEBUG -m run_test -s ${suite} all -a print -w "config==nbif_all_rtl && when=~/nbif_nightly/"\n';
+        text += 'dj -l testlist.log -DDEBUG -m run_test -s nbifall all -a print -w "config==nbif_all_rtl && when=~/nbif_nightly/"\n';
         text += 'bdji -l build.log -m -DREGRESS -DUSE_VRQ -DCGM run_test -s nbifall demo_test_0_nbif_all_rtl -a execute=off\n';
         text += 'bdji -l run.log -DRERUN_TDL_BATCH=$batch_name_v -m -DREGRESS -DUSE_VRQ -DCGM run_test -s nbifall all -b trs -A trs.batch=plsignore -A trs.environment=nbif_al_gpu -A trs.cec.logspec='+treeRoot+'/_env/local/nbif_logspec.xml -A trs.switches="-regr-no-results-copy" -w "config==nbif_all_rtl && when=~/nbif_nightly/" -a run_only\n';//FIXME about the -s arg
         text += 'echo "done"\n';
@@ -187,6 +170,7 @@ var jobid_regression_main_daily = new cronJob('0 23 13 * * *',function(){
             console.log(error);
           }
           console.log(stdout);
+          jobid_regression_main_daily_check_status.start();
         });
       }
       else if(JSON.parse(chunk).ok == 'notok'){
