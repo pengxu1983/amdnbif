59d8c80276cbb4bb6b3bde01c5faca51d3fa048b
pengxu1983 <pengxu1983@163.com>
2019-03-13 22:06:00 +0800

    dbg

--- nbif1.0/server/api/controllers/regression/uploadstatus.js
+++ nbif1.0/server/api/controllers/regression/uploadstatus.js
@@ -1,5 +1,7 @@
 var querystring   = require('querystring');
 var moment        = require('moment');
+var http          = require('http');
+var child_process = require('child_process');
 var cronJob       = require("cron").CronJob;
 var jobid_cal_passingrates  = new cronJob('0 */30 * * * *',function(){
   console.log('jobid_cal_passingrates start at '+moment().format('YYYY-MM-DD HH:mm:ss'));
