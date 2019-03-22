3906e0901b752a9c2e82780138227ceae4d29e65
pengxu1983 <pengxu1983@163.com>
2019-03-22 12:17:19 +0800

    dbg

--- nbif1.0/web/main/src/components/SanityPage_sub/SanityPage_config_main.vue
+++ nbif1.0/web/main/src/components/SanityPage_sub/SanityPage_config_main.vue
@@ -32,6 +32,65 @@
                 label="Valid"
               >
                 <template slot-scope="scope">
+                  <el-checkbox-group v-model="scope.row.valid.NV21" >
+                    <div 
+                      v-for="onevariant in variants" 
+                    >
+                      <el-checkbox :label="onevariant.variantname"></el-checkbox>           
+                    </div>
+                  </el-checkbox-group>
+                </template>
+              </el-table-column>
+              <el-table-column
+                fixed="right"
+                label="operation"
+                width="120"
+              >
+                <template slot-scope="scope">
+                  <el-button
+                    @click.native.prevent="deleteRow(scope.$index, common_sanitys)"
+                    type="text"
+                    size="small"
+                  >
+                    Delete
+                  </el-button>
+                </template>
+              </el-table-column>
+            </el-table>
+          </el-main>
+        </el-container>
+      </template>
+      <template>
+        <el-container>
+          <el-header>
+            <el-row>
+              <el-button type="primary" round>Add</el-button>
+              <el-button type="primary" round>Upload</el-button>
+            </el-row>
+          </el-header>
+          <el-main>
+            <el-table
+              :data="common_sanitys"
+              border
+              style="width: 100%"
+            >
+              <el-table-column
+                prop="testname"
+                label="TestName"
+              >
+                <template slot-scope="scope">
+                  <el-input
+                    placeholder="TestName"
+                    v-model="scope.row.testname"
+                    clearable>
+                  </el-input>
+                </template>
+              </el-table-column>
+              <el-table-column
+                prop="valid"
+                label="Valid"
+              >
+                <template slot-scope="scope">
                   <el-checkbox-group v-model="scope.row.valid.MAIN" >
                     <div 
                       v-for="onevariant in variants" 
