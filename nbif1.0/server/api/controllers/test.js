ec059dce22a4d80241669b5f9415388982b7798b
pengxu1983 <pengxu1983@163.com>
2019-03-30 10:01:54 +0800

    dbg

--- nbif1.0/server/api/controllers/test.js
+++ nbif1.0/server/api/controllers/test.js
@@ -10,6 +10,9 @@ module.exports = {
   inputs: {
     kind  : {
       type  : 'string'
+    },
+    tree  : {
+      type  : 'string'
     }
   },
 
@@ -19,10 +22,22 @@ module.exports = {
   },
 
 
-  fn: async function (inputs) {
-    
+  fn: async function (inputs,exits) {
+    let R = await Common_sanitys.find({
+      id  : {'>=':0}
+    });
+    for(let t = 0;t<R.length;t++){
+      if(t == 0){
+        let a = R[t];
+        let b = typeof(R[t]);
+        return exits.success(JSON.stringify({
+          ok  : 'ok',
+          a   : a,
+          b   : b
+        }));
+      }
+    }
     // All done.
-    return;
 
   }
 
