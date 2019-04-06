d7626c82596b0195d78b179e026dce7a4bd93edd
pengxu1983 <pengxu1983@163.com>
2019-04-06 20:55:25 +0800

    dbg

--- nbif1.0/server/api/controllers/sanitys/dcelab/uploadstatus.js
+++ nbif1.0/server/api/controllers/sanitys/dcelab/uploadstatus.js
@@ -33,6 +33,7 @@ module.exports = {
     sails.log('/sanitys/dcelab/uploadstatus');
     sails.log(inputs);
     let results = JSON.parse(inputs.results);
+    sails.log(results);
     if(inputs.tree == 'MAIN'){
       if(inputs.kind == 'dcelabuploadstatus'){
         //all variants
@@ -46,7 +47,7 @@ module.exports = {
         let currentChangelistIsBroken = 'no';
         for(let v=0;v<variants.length;v++){
           for(let t=0;t<tasks.length;t++){
-            if(results[variants[v].variantname][tasks[t].taskname] == 'FAIL'){
+            if(results[tasks[t].taskname][variants[v].variantname] == 'FAIL'){
               let valid = JSON.parse(tasks[t].valid);
               if(valid[inputs.tree].indexOf(variants[v].variantname)==-1){
                 //ignored
@@ -118,7 +119,90 @@ module.exports = {
         }));
       }
     }
+    if(inputs.tree == 'NV21'){
+      if(inputs.kind == 'dcelabuploadstatus'){
+        //all variants
+        let variants  = await Variants.find({
+          id  : {'>=':0}
+        });
+        //all tests
+        let tasks = await Common_tasks.find({
+          id  : {'>=':0}
+        });
+        let currentChangelistIsBroken = 'no';
+        for(let v=0;v<variants.length;v++){
+          for(let t=0;t<tasks.length;t++){
+            if(results[tasks[t].taskname][variants[v].variantname] == 'FAIL'){
+              let valid = JSON.parse(tasks[t].valid);
+              if(valid[inputs.tree].indexOf(variants[v].variantname)==-1){
+                //ignored
+                sails.log('ignored');
+                sails.log(variants[v].variantname);
+              }
+              else{
+                currentChangelistIsBroken = 'yes';
+              }
+            }
+          }
+        }
+        //check last CL if broken
+        let brokenCLs = await Buffer_changelists_01.find({
+          dcelabisBroken  : 'yes'
+        });
+        let checkedCLs = await Buffer_changelists_01.find({
+          dcelabischecked : 'yes',
+          dcelab  : {'!=':'NA'}
+        });
+        let brokenCL;
+        if(brokenCLs.length == 0){// no changelist broken before
+          if(currentChangelistIsBroken == 'yes'){
+            //send email
+            //set brokenCL 
+            brokenCL = inputs.changelist;
+          }
+          else if(currentChangelistIsBroken == 'no'){
+            //nothing
+            brokenCL = 'NA';
+          }
+        }
+        else{
+          if(currentChangelistIsBroken == 'yes'){
+            //find checked latest CL
+            let lastCL;
+            for(let s=0;s<checkedCLs.length;s++){
+              if(s==0){
+                lastCL = checkedCLs[s];
+              }
+              else{
+                if(parseInt(checkedCLs[s].changelist)>parseInt(lastCL.changelist)){
+                  lastCL = checkedCLs[s];
+                }
+              }
+            }
+            if(lastCL.isBroken == 'yes'){
+              //nothing since previous CL is broken and already send mail
+              brokenCL = lastCL.brokenCL;
+            }
+            else if(lastCL.isBroken == 'no'){
+              //send email
+              brokenCL = inputs.changelist;
+            }
+          }
+          else if(currentChangelistIsBroken == 'no'){
+            //nothing
+            brokenCL = 'NA';
+          }
+        }
+        await Buffer_changelists_01.update({
+          changelist  : inputs.changelist
+        },{
+          dcelab  : inputs.results,
+          dcelabisBroken  : currentChangelistIsBroken
+        });
+        return exits.success(JSON.stringify({
+          ok  : 'ok'
+        }));
+      }
+    }
   }
-
-
 };
