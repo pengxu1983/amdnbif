11477691800858f41c4125e1e4f914c493714bb5
pengxu1983 <pengxu1983@163.com>
2019-04-07 19:33:06 +0800

    dbg

--- nbif1.0/server/api/controllers/sanitys/common-sanity/getcommonsanitystatus.js
+++ nbif1.0/server/api/controllers/sanitys/common-sanity/getcommonsanitystatus.js
@@ -10,6 +10,9 @@ module.exports = {
   inputs: {
     kind  : {
       type  : 'string'
+    },
+    tree  : {
+      type  : 'string'
     }
   },
 
@@ -23,63 +26,159 @@ module.exports = {
     sails.log('/sanitys/common-sanity/getcommonsanitystatus');
     sails.log(inputs);
     if(inputs.tree  ==  'MAIN'){
-      if(inputs.kind == 'sanityStatus'){
-        sails.log('on');
-        let results = await Buffer_changelists.find({
-          ischecked : 'yes',
-          results   : {'!=':'NA'}
-        });
-        let resultsToPop = results;
-        if(results.length == 0){
-          return exits.success(JSON.stringify({
-            ok  : 'notok',
-            msg : 'no result found'
-          }));
-        }
-        else {
-          //find latest
-          let lastcheckedCL ;
-          for(let r=0;r<results.length;r++){
-            if(r==0){
-              lastcheckedCL = results[r];
-            }
-            else if(parseInt(results[r].changelist)>parseInt(lastcheckedCL.changelist)){
-              lastcheckedCL = results[r];
-            }
-          }
-          if(lastcheckedCL.brokenCL != 'NA'){
-            let brokenCL = await Buffer_changelists.findOne({
-              changelist  : lastcheckedCL.brokenCL
-            });
-            return exits.success(JSON.stringify({
-              ok  : 'ok',
-              lastcheckedCL: lastcheckedCL.changelist,
-              result    : lastcheckedCL.isBroken,
-              brokenCL  : lastcheckedCL.brokenCL,
-              brokenCLowner : brokenCL.owner,
-              dcelab    : lastcheckedCL.dcelab,
-              details   : lastcheckedCL.results
-            }));
-          }
-          else if(lastcheckedCL.brokenCL == 'NA'){
-            return exits.success(JSON.stringify({
-              ok  : 'ok',
-              lastcheckedCL: lastcheckedCL.changelist,
-              result    : lastcheckedCL.isBroken,
-              brokenCL  : lastcheckedCL.brokenCL,
-              brokenCLowner : 'NA',
-
-              //dcelab    : lastcheckedCL.dcelab,
-              details   : lastcheckedCL.results
-            }));
-          }
-
-        }
+      let sanity_lastcheckedCL='NA';
+      let sanity_result='NA';
+      let sanity_lastpassCL='NA';
+      let sanity_brokenCL='NA';
+      let sanity_brokenCLowner='NA';
+      let sanity_details;
+      let dcelab_lastcheckedCL='NA';
+      let dcelab_result='NA';
+      let dcelab_lastpassCL='NA';
+      let dcelab_brokenCL='NA';
+      let dcelab_brokenCLowner='NA';
+      let dcelab_details;
+      //=============================================
+      ///////////////////////////
+      //sanity_lastcheckedCL
+      ///////////////////////////
+      let R = await Buffer_changelists.find({//TODOTREE
+        ischecked : 'yes',
+        results   : {'!=':'NA'}
+      });
+      if(R.length == 0){
       }
+      else{
+        for(let r=0;r<R.length;r++){
+          if(r==0){
+            sanity_lastcheckedCL  = R[r].changelist;
+          }
+          else if(parseInt(sanity_lastcheckedCL)<parseInt(R[r].changelist)){
+            sanity_lastcheckedCL  = R[r].changelist;
+          }
+          ///////////////////////////
+          //sanity_result
+          ///////////////////////////
+          if(R[r].isBroken  ==  'no'){
+            sanity_result = 'PASS';
+            ///////////////////////////
+            //sanity_lastpassCL
+            ///////////////////////////
+            sanity_lastpassCL = sanity_lastcheckedCL;
+          }
+          else if(R[r].isBroken  ==  'yes'){
+            sanity_result = 'FAIL';
+            ///////////////////////////
+            //sanity_lastpassCL
+            ///////////////////////////
+            let RR = await Buffer_changelists.find({//TODOTREE
+              ischecked : 'yes',
+              isBroken  : 'no'
+            });
+            for(let rr=0;rr<RR.length;rr++){
+              if(rr==0){
+                sanity_lastpassCL = RR[rr].changelist;
+              }
+              else if(parseInt(sanity_lastpassCL)<parseInt(RR[rr].changelist)){
+                sanity_lastpassCL = RR[rr].changelist;
+              }
+            }
+            ///////////////////////////
+            //sanity_brokenCL
+            ///////////////////////////
+            sanity_brokenCL = R[r].brokenCL;
+            ///////////////////////////
+            //sanity_brokenCLowner
+            ///////////////////////////
+            RR = await Buffer_changelists.findOne({//TODOTREE
+              changelist  : sanity_brokenCL
+            });
+            sanity_brokenCLowner  = RR.owner;
+          }
+        }
+        ///////////////////////////
+        //sanity_result
+        ///////////////////////////
+        sanity_result = R.results;
+      }
+      //=============================================
+      ///////////////////////////
+      //dcelab_lastcheckedCL
+      ///////////////////////////
+      R = await Buffer_changelists.find({//TODOTREE
+        dcelabischecked : 'yes',
+        dcelab          : {'!=':'NA'}
+      });
+      if(R.length == 0){
+      }
+      else{
+        for(let r=0;r<R.length;r++){
+          if(r==0){
+            dcelab_lastcheckedCL  = R[r].changelist;
+          }
+          else if(parseInt(dcelab_lastcheckedCL)<parseInt(R[r].changelist)){
+            dcelab_lastcheckedCL  = R[r].changelist;
+          }
+          ///////////////////////////
+          //dcelab_result
+          ///////////////////////////
+          if(R[r].dcelabisBroken  ==  'no'){
+            dcelab_result = 'PASS';
+            ///////////////////////////
+            //dcelab_lastpassCL
+            ///////////////////////////
+            dcelab_lastpassCL = dcelab_lastcheckedCL;
+          }
+          else if(R[r].dcelabisBroken  ==  'yes'){
+            dcelab_result = 'FAIL';
+            ///////////////////////////
+            //dcelab_lastpassCL
+            ///////////////////////////
+            let RR = await Buffer_changelists.find({//TODOTREE
+              dcelabischecked : 'yes',
+              dcelabisBroken  : 'no'
+            });
+            for(let rr=0;rr<RR.length;rr++){
+              if(rr==0){
+                dcelab_lastpassCL = RR[rr].changelist;
+              }
+              else if(parseInt(dcelab_lastpassCL)<parseInt(RR[rr].changelist)){
+                dcelab_lastpassCL = RR[rr].changelist;
+              }
+            }
+            ///////////////////////////
+            //dcelab_brokenCL
+            ///////////////////////////
+            dcelab_brokenCL = R[r].dcelabbrokenCL;
+            ///////////////////////////
+            //dcelab_brokenCLowner
+            ///////////////////////////
+            RR = await Buffer_changelists.findOne({//TODOTREE
+              changelist  : dcelab_brokenCL
+            });
+            dcelab_brokenCLowner  = RR.owner;
+          }
+        }
+        ///////////////////////////
+        //dcelab_result
+        ///////////////////////////
+        dcelab_result = R.dcelab;
+      }
+      return exits.success(JSON.stringify({
+        ok                    : 'ok',
+        sanity_lastcheckedCL  : sanity_lastcheckedCL ,
+        sanity_result         : sanity_result        ,
+        sanity_lastpassCL     : sanity_lastpassCL    ,
+        sanity_brokenCL       : sanity_brokenCL      ,
+        sanity_brokenCLowner  : sanity_brokenCLowner ,
+        sanity_details        : sanity_details       ,
+        dcelab_lastcheckedCL  : dcelab_lastcheckedCL ,
+        dcelab_result         : dcelab_result        ,
+        dcelab_lastpassCL     : dcelab_lastpassCL    ,
+        dcelab_brokenCL       : dcelab_brokenCL      ,
+        dcelab_brokenCLowner  : dcelab_brokenCLowner ,
+        dcelab_details        : dcelab_details        
+      }));
     }
-    // All done.
-
   }
-
-
 };
--- nbif1.0/web/main/src/components/SanityPage_sub/SanityPage_status_main.vue
+++ nbif1.0/web/main/src/components/SanityPage_sub/SanityPage_status_main.vue
@@ -73,7 +73,7 @@ export default {
   data() {
     return {
       activeTab   : 'MAIN',
-      sanityStatus: [],
+      sanityStatus: {},
       sanitys     : [],
       dcelabStatus: [],
       projectinfo : {
@@ -124,80 +124,11 @@ export default {
         tree  : tree
       }).then(
         function(response){
-          if(response.body.ok == 'ok'){
-            this.sanityStatus = [];
-            let result;
-            if(response.body.result == 'no'){
-              result = 'PASS'
-            }
-            else if(response.body.result == 'yes'){
-              result = 'FAIL'
-            }
-            let details = [];
-            let respdetails = response.body.details;
-            console.log(JSON.parse(respdetails));
-            console.log(typeof(JSON.parse(respdetails)));
-            let R = JSON.parse(respdetails);
-            let tests = ['demo_test_0','demo_test_1','demo_test_2']
-            for(let detailsvariant in R){
-              let one = {};
-              one['variantname']  = detailsvariant;
-              for(let t=0;t<tests.length;t++){
-                one[tests[t]] = R[detailsvariant][tests[t]];
-              }
-              details.push(one);
-            }
-            console.log(typeof(details));
-            console.log(details);
-            this.sanityStatus.push({
-              lastcheckedCL : response.body.lastcheckedCL,
-              result        : result,
-              brokenCL      : response.body.brokenCL,
-              brokenCLowner : response.body.brokenCLowner,
-              dcelab        : response.body.dcelab,
-              //details       : response.body.details
-              details       : details
-            });
-            console.log(this.sanityStatus);
-            let DCelabresult = [];
-            console.log('aaa');
-            console.log(response.body.dcelab);
-            console.log(typeof(response.body.dcelab));
-            if(response.body.dcelab == 'NA'){
-              console.log('bbb');
-              DCelabresult.push({
-                variantname : 'Not Done',
-                result  : 'NOT Done'
-              });
-            }
-            else{
-              console.log('ccc');
-              R = JSON.parse(response.body.dcelab);
-              for(let onevariant in R){
-                let cmd = 'dj -v -l dc_elab.log -e \'releaseflow::dropflow(:rtl_drop).build(:rhea_drop,:rhea_dc)\' -DPUBLISH_BLKS=nbif_shub_wrap_';
-                if(onevariant == 'nbif_al_gpu'){
-                  cmd += 'algfx\n';
-                }
-                if(onevariant == 'nbif_oak_gpu'){
-                  cmd += 'oakgfx\n';
-                }
-                if(onevariant == 'nbif_nv10_gpu'){
-                  cmd += 'gfx -DDISABLE_VDCI2 -DDISABLE_REGSLICE\n';
-                }
-                if(onevariant == 'nbif_ssp_ntb'){
-                  cmd += 'ntb\n';
-                }
-                if(onevariant == 'nbif_ssp_generic_a'){
-                  cmd += 'rc_a\n';
-                }
-                DCelabresult.push({
-                  variantname : onevariant,
-                  result      : R[onevariant],
-                  command     : cmd
-                });
-              }
-            }
-            this.dcelabStatus = DCelabresult;
+          if(response.body.ok ==  'ok'){
+            console.log(response.body);
+          }
+          else if(response.body.ok ==  'notok'){
+            console.log(tree+' sanityStatus route is notok');
           }
         },
         function(){}
