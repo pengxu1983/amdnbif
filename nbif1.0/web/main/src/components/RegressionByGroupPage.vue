b90d152bd13e5b0ee27db0c3ffa245793734d977
pengxu1983 <pengxu1983@163.com>
2019-05-27 21:29:29 +0800

    initial version of nbif management web

--- nbif1.0/web/main/src/components/RegressionByGroupPage.vue
+++ nbif1.0/web/main/src/components/RegressionByGroupPage.vue
@@ -1,9 +1,15 @@
 <template>
   <el-container>
-    <nav class="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
-      <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="/main">MainPage</a>
-      <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="#">RegressionByGroup</a>
-    </nav>
+    <el-header>
+      <nav class="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
+        <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="/main">MainPage</a>
+        <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="#">RegressionByGroup</a>
+      </nav>
+    </el-header>
+    <el-container>
+      <el-aside width="200px">Aside</el-aside>
+      <el-main>Main</el-main>
+    </el-container>
   </el-container>
 </template>
 
