93cfdb210d6e07c5002814d2022633ab94f24796
pengxu1983 <pengxu1983@163.com>
2019-05-31 21:28:06 +0800

    clean up

--- nbif_mng_1.0/web/vue.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  outputDir: '/mnt/servers/amdnbif/nbif_mng_1.0/server/assets/main',
-  baseUrl: '/main',
-  assetsDir: undefined,
-  runtimeCompiler: undefined,
-  productionSourceMap: undefined,
-  parallel: undefined,
-  css: undefined
-}
