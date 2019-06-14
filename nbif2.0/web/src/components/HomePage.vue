98fbce1b44d5e4cf592d88cbe2ecc5910258cc84
pengxu1983 <pengxu1983@163.com>
2019-06-14 23:03:12 +0800

    dbg

---
+++ nbif2.0/web/src/components/HomePage.vue
@@ -0,0 +1,54 @@
+<template>
+  <el-carousel :interval="4000" type="card" height="400px">
+    <el-carousel-item 
+      v-for="item in items" 
+      :key="item"
+      >
+      <a @click="gotourl(item)"><h3 class="medium">{{ item }}</h3></a>
+    </el-carousel-item>
+  </el-carousel>
+</template>
+
+<script>
+export default {
+  name: 'HomePage',
+  props: {
+  },
+  data () {
+    return {
+      items : [
+        'sanity',
+        'regression',
+        'action'
+      ]
+    }
+  },
+  methods : {
+    gotourl (url) {
+      console.log('going to url '+url);
+      this.$router.push({
+        name  : url
+      });
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .el-carousel__item h3 {
+    color: #475669;
+    font-size: 14px;
+    opacity: 0.75;
+    line-height: 400px;
+    margin: 0;
+  }
+  
+  .el-carousel__item:nth-child(2n) {
+    background-color: #99a9bf;
+  }
+  
+  .el-carousel__item:nth-child(2n+1) {
+    background-color: #d3dce6;
+  }
+</style>
