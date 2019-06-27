d8db8307bf9e99677c870b505c151639c1de3dbb
pengxu1983 <pengxu1983@163.com>
2019-06-27 23:36:42 +0800

    dbg

--- nbif3.0/srv/api/controllers/regression/get.js
+++ nbif3.0/srv/api/controllers/regression/get.js
@@ -41,7 +41,7 @@ module.exports = {
           return exits.success(JSON.stringify({
             ok  : 'ok',
             regressionstatus  : R
-          });
+          }));
         }
       }
     }
