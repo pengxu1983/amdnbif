daca7786fb8f2283e91dd4cedcc221114b1c856f
pengxu1983 <pengxu1983@163.com>
2019-07-12 22:03:08 +0800

    dbg

--- nbif3.0/web/src/components/ConfigPage_sub/VariantsPage.vue
+++ nbif3.0/web/src/components/ConfigPage_sub/VariantsPage.vue
@@ -1,10 +1,5 @@
 <template>
   <el-container>
-    <el-header>
-      <nav class="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
-        <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="/">NBIF Main Page</a>
-      </nav>
-    </el-header>
     <el-main>
       <el-row>
         <el-button 
@@ -21,7 +16,7 @@
         border
         :data="variants"
         style="width: 100%"
-        max-height="250">
+      >
         <el-table-column
           fixed
           prop="variantname"
