622cc1e0a31eb12fa60a8de0d764c832edb2f839
pengxu1983 <pengxu1983@163.com>
2019-08-05 06:05:31 -0400

    dbg

--- nbif3.0/clt/clt_regression/api/controllers/mi-200/check-apu.js
+++ nbif3.0/clt/clt_regression/api/controllers/mi-200/check-apu.js
@@ -108,7 +108,7 @@ let cron_send_request = new cronJob('* * * * * *',function(){
     postQ.splice(0,indexmax);
   }
 },null,false,'Asia/Chongqing');
-let cron_check_result = new cronJob('0 15 */3 * * *',function(){
+let cron_check_result = new cronJob('0 25 11 * * *',function(){
   console.log('cron_check_result starts at '+moment().format('YYYY-MM-DD HH:mm:ss'));
   console.log('basic info :');
   console.log('refTreeRoot is '+refTreeRoot);
