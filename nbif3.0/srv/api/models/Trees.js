7dba164645f75c9b752b17644dd841f4536203ae
pengxu1983 <pengxu1983@163.com>
2019-08-08 11:25:37 +0800

    dbg

--- nbif3.0/srv/api/models/Trees.js
+++ nbif3.0/srv/api/models/Trees.js
@@ -36,6 +36,9 @@ module.exports = {
     checkmask   : {
       type  : 'string',
       columnType  : 'longtext'
+    },
+    treename  : {
+      type  : 'string'
     }
 
     //  ╔═╗╔╦╗╔╗ ╔═╗╔╦╗╔═╗
