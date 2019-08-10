1d5ca87c604118b16f65aeac6299d21b54566229
pengxu1983 <pengxu1983@163.com>
2019-08-10 11:32:35 +0800

    dbg

---
+++ nbif3.0/srv/api/controllers/sanity/get.js
@@ -0,0 +1,28 @@
+module.exports = {
+
+
+  friendlyName: 'Get',
+
+
+  description: 'Get sanity.',
+
+
+  inputs: {
+
+  },
+
+
+  exits: {
+
+  },
+
+
+  fn: async function (inputs) {
+
+    // All done.
+    return;
+
+  }
+
+
+};
