046f2642fb554aef8b86ec6fd01dc17d6ee83373
pengxu1983 <pengxu1983@163.com>
2019-08-18 20:09:18 +0800

    dbg

--- nbif3.0/web/src/components/RegressionPage_sub/Overall_sub/OneprojPage_sub/Oneprojoneregress.vue
+++ nbif3.0/web/src/components/RegressionPage_sub/Overall_sub/OneprojPage_sub/Oneprojoneregress.vue
@@ -1,29 +1,37 @@
 <template>
   <div>
-    <el-table
-      :data="groupstatus"
-      stripe
-      border
-      style="width: 100%">
-      <el-table-column
-        prop="DVgroup"
-        label="DVgroup"
-        sortable
+    <el-tabs type="border-card">
+      <el-tab-pane 
+        v-for="oneDVgroup in DVgroups"
+        :label="oneDVgroup"
       >
-      </el-table-column>
-      <el-table-column
-        prop="groupname"
-        label="groupname"
-        sortable
-      >
-      </el-table-column>
-      <el-table-column
-        prop="passrate"
-        label="passrate"
-        sortable
-      >
-      </el-table-column>
-    </el-table>
+        <el-table
+          :data="groupstatus_split(oneDVgroup)"
+          stripe
+          border
+          style="width: 100%">
+          <el-table-column
+            prop="DVgroup"
+            label="DVgroup"
+            sortable
+          >
+          </el-table-column>
+          <el-table-column
+            prop="groupname"
+            label="groupname"
+            sortable
+          >
+          </el-table-column>
+          <el-table-column
+            prop="passrate"
+            label="passrate"
+            sortable
+          >
+          </el-table-column>
+        </el-table>
+      </el-tab-pane>
+    </el-tabs>
+    
   </div>
 </template>
 
@@ -34,8 +42,27 @@ export default {
     groupstatus : []
   },
   data() {
+    return {
+      DVgroups  : [
+        'HOST',
+        'DMA',
+        'MISC',
+        'OTHERS',
+        'PERF'
+      ],//TODO need to fetch from DB
+    }
   },
   methods : {
+    groupstatus_split(DVgroupname){
+      let R = [];
+      for(let g=0;g<this.groupstatus.length;g++){
+        if(this.groupstatus[g].DVgroup  ==  DVgroupname){
+          R.push(this.groupstatus[g]);
+        }
+        else{
+        }
+      }
+    }
   },
   mounted (){
   }
