06090ded014d0f2bb1777f8004f7811a41138557
pengxu1983 <pengxu1983@163.com>
2019-09-09 21:00:40 +0800

    dbg

--- nbif3.0/srv/api/models/Regressionneverpass0004.js
+++ nbif3.0/srv/api/models/Regressionneverpass0004.js
@@ -1,5 +1,5 @@
 /**
- * Regressionneverpass0004.js
+ * Regressionneverpass0001.js
  *
  * @description :: A model definition represents a database table/collection.
  * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
@@ -12,8 +12,33 @@ module.exports = {
     //  ╔═╗╦═╗╦╔╦╗╦╔╦╗╦╦  ╦╔═╗╔═╗
     //  ╠═╝╠╦╝║║║║║ ║ ║╚╗╔╝║╣ ╚═╗
     //  ╩  ╩╚═╩╩ ╩╩ ╩ ╩ ╚╝ ╚═╝╚═╝
-
-
+    projectname : {
+      type      : 'string'
+    },
+    variantname : {
+      type      : 'string'
+    },
+    testname    : {
+      type      : 'string'
+    },
+    seed        : {
+      type      : 'string'
+    },
+    isBAPU      : {
+      type      : 'string'
+    },
+    suite       : {
+      type      : 'string'
+    },
+    owner       : {
+      type      : 'string'
+    },
+    fixETA      : {
+      type      : 'string'
+    },
+    lastfail    : {
+      type      : 'string'
+    }
     //  ╔═╗╔╦╗╔╗ ╔═╗╔╦╗╔═╗
     //  ║╣ ║║║╠╩╗║╣  ║║╚═╗
     //  ╚═╝╩ ╩╚═╝╚═╝═╩╝╚═╝
