702e0f74476afe97cd3c2dbfdede9fd31eac5e91
pengxu1983 <pengxu1983@163.com>
2019-09-22 14:41:15 +0800

    dbg

--- nbif3.0/srv/api/controllers/regression/dvgroupsummary.js
+++ nbif3.0/srv/api/controllers/regression/dvgroupsummary.js
@@ -118,14 +118,12 @@ module.exports = {
     //--------------------------------
     //update summary : start
     //--------------------------------
-    let W0 = {
+    let W0  = {
       projectname : inputs.projectname,
       variantname : inputs.variantname,
       isBAPU      : inputs.isBAPU,
-      kickoffdate : inputs.kickoffdate,
-      changelist  : inputs.changelist,
-      shelve      : inputs.shelve,
     };
+
     if(alldvgroups.length == 0){
       return exits.success(JSON.stringify({
         ok  : 'notok',
@@ -133,6 +131,7 @@ module.exports = {
       }));
     }
     else{
+      let W1  = JSON.parse(JSON.stringify(W0));//dvgroupsummary
       for(let g=0;g<alldvgroups.length;g++){
         sails.log('dvgroup');
         sails.log(alldvgroups[g]);
@@ -151,41 +150,87 @@ module.exports = {
           'unknownlist',
           'runninglist'
         ];
+        W1.DVgroup      = alldvgroups[g];
+        W1.kickoffdate  = inputs.kickoffdate;
+        W1.changelist   = inputs.changelist;
+        W1.shelve       = inputs.shelve;
         //Get all groupname under this DVgroup
-        let R = await Groups.find({
-          projectname : inputs.projectname,
-          variantname : inputs.variantname,
-          isBAPU      : inputs.isBAPU,
-          DVgroup     : alldvgroups[g]
-        });
+        let R = await Groups.find(W1);
         sails.log('all groups');
         sails.log(R);
+        let W2  = JSON.parse(JSON.stringify(W0));//summary
+        W2.kickoffdate  = inputs.kickoffdate;
+        W2.changelist   = inputs.changelist;
+        W2.shelve       = inputs.shelve;
         for(let r=0;r<R.length;r++){
-          //one groupname
-          let R1 = await Regressiondvgroup0001.findOne({
-            projectname : inputs.projectname,
-            variantname : inputs.variantname,
-            isBAPU      : inputs.isBAPU,
-            kickoffdate : inputs.kickoffdate,
-            changelist  : inputs.changelist,
-            shelve      : inputs.shelve,
-          });
+          W2.groupname    = R[r].groupname;
+          sails.log('W2');
+          sails.log(W2);
+          let R1;
+          ////////////////////////////////
+          //For 0001 start
+          ////////////////////////////////
+          if(inputs.projectname ==  'mi200'){
+            R1 = await Regressionsummary0001.findOne(W2);
+          }
+          ////////////////////////////////
+          //For 0001 end
+          ////////////////////////////////
+          ////////////////////////////////
+          //For 0002 start
+          ////////////////////////////////
+          if(inputs.projectname ==  'mero'){
+            R1 = await Regressionsummary0002.findOne(W2);
+          }
+          ////////////////////////////////
+          //For 0002 end
+          ////////////////////////////////
+          ////////////////////////////////
+          //For 0003 start
+          ////////////////////////////////
+          if(inputs.projectname ==  'rembrandt'){
+            R1 = await Regressionsummary0003.findOne(W2);
+          }
+          ////////////////////////////////
+          //For 0003 end
+          ////////////////////////////////
+          ////////////////////////////////
+          //For 0004 start
+          ////////////////////////////////
+          if(inputs.projectname ==  'floyd'){
+            R1 = await Regressionsummary0004.findOne(W2);
+          }
+          ////////////////////////////////
+          //For 0004 end
+          ////////////////////////////////
+          sails.log('R1');
+          sails.log(R1);
           testlist    +=  Number(R1.testlist);
           passlist    +=  Number(R1.passlist);
           faillist    +=  Number(R1.faillist);
           unknownlist +=  Number(R1.unknownlist);
           runninglist +=  Number(R1.runninglist);
         }
+        let passrate = 0.00;
+        passrate  = passlist/testlist*100;
+        passrate  = passrate.toFixed(2);
+        let checkedtime = moment().format('YYYY-MM-DD HH:mm:ss');
+        sails.log('passrate');
+        sails.log(passrate);
+        sails.log('W1b');
+        sails.log(W1);
+        await Regressiondvgroup0001.destroy(W1);
+        W1.testlist    = testlist   ;
+        W1.passlist    = passlist   ;
+        W1.faillist    = faillist   ;
+        W1.unknownlist = unknownlist;
+        W1.runninglist = runninglist;
+        W1.passrate    = passrate   ;
+        W1.checkedtime = checkedtime;
+        await Regressiondvgroup0001.create(W1);
       }
-      let passrate = 0.00;
-      passrate  = passlist/testlist*100;
-      passrate  = passrate.toFixed(2);
-      let checkedtime = moment().format('YYYY-MM-DD HH:mm:ss');
-      sails.log('passrate');
-      sails.log(passrate);
       return exits.success(JSON.stringify({
         ok  : 'ok',
-        msg : 'updated all at '+ moment().format('YYYY-MM-DD HH:mm:ss')
       }));
     }
   }
