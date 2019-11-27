f39c9669d08a6c70a7408fd62df17bdfea5fce2e
pengxu1983 <pengxu1983@163.com>
2019-11-27 23:59:26 +0800

    dbg

--- nbif3.0/web/src/components/RegressionPage_sub/FunctionCoverage.vue
+++ nbif3.0/web/src/components/RegressionPage_sub/FunctionCoverage.vue
@@ -2,8 +2,8 @@
   <el-container>
     <el-header>
       <el-row>
-        <el-button type="primary"><a target="abc" href="logviewer-atl/proj/cip_floyd_genz/ip_regression/nbif.regression.main.mi200.nbif_nv10_gpu.group1.1/func_cov_report/dashboard.html">MI200 dgpu</a></el-button>
-        <el-button type="primary"><a target="abc" href="logviewer-atl/proj/cip_floyd_genz/ip_regression/nbif.regression.main.mi200.nbif_nv10_gpu.bapu.group1.1/func_cov_report/dashboard.html">MI200 bapu</a></el-button>
+        <el-button type="primary"><a target="abc" href="http://logviewer-atl/proj/cip_floyd_genz/ip_regression/nbif.regression.main.mi200.nbif_nv10_gpu.group1.1/func_cov_report/dashboard.html">MI200 dgpu</a></el-button>
+        <el-button type="primary"><a target="abc" href="http://logviewer-atl/proj/cip_floyd_genz/ip_regression/nbif.regression.main.mi200.nbif_nv10_gpu.bapu.group1.1/func_cov_report/dashboard.html">MI200 bapu</a></el-button>
       </el-row>
     </el-header>
     <el-main>
