23229c0a22899e5ba5dd6627611d69c84c8c6aeb
pengxu1983 <pengxu1983@163.com>
2020-01-08 16:26:47 +0800

    dbg

--- nbif3.0/web/src/components/RegressionPage_sub/FunctionCoverage.vue
+++ nbif3.0/web/src/components/RegressionPage_sub/FunctionCoverage.vue
@@ -2,8 +2,12 @@
   <el-container>
     <el-header>
       <el-row>
-        <el-button ><a target="abc" href="http://logviewer-atl/proj/cip_floyd_genz/ip_regression/nbif.regression.main.mi200.nbif_nv10_gpu.group1.1/func_cov_report/dashboard.html">MI200 dgpu</a></el-button>
-        <el-button ><a target="abc" href="http://logviewer-atl/proj/cip_floyd_genz/ip_regression/nbif.regression.main.mi200.nbif_nv10_gpu.bapu.group1.1/func_cov_report/dashboard.html">MI200 bapu</a></el-button>
+        <el-button ><a target="abc" href="http://logviewer-atl/proj/cip_floyd_genz/ip_regression/nbif.regression.branch.mi200.nbif_nv10_gpu.group1.1/func_cov_report/dashboard.html">MI200 dgpu</a></el-button>
+        <el-button ><a target="abc" href="http://logviewer-atl/proj/cip_floyd_genz/ip_regression/nbif.regression.branch.mi200.nbif_nv10_gpu.bapu.group1.1/func_cov_report/dashboard.html">MI200 bapu</a></el-button>
+        <el-button ><a target="abc" href="http://logviewer-atl/proj/cip_floyd_genz/ip_regression/nbif.regression.main.rembrandt.nbif_draco_gpu.group1.1/func_cov_report/dashboard.html">RMB</a></el-button>
+        <el-button ><a target="abc" href="http://logviewer-atl/proj/cip_floyd_genz/ip_regression/nbif.regression.main.floyd.nbif_et_0.group1.1/func_cov_report/dashboard.html">Floyd et0</a></el-button>
+        <el-button ><a target="abc" href="http://logviewer-atl/proj/cip_floyd_genz/ip_regression/nbif.regression.main.floyd.nbif_et_1.group1.1/func_cov_report/dashboard.html">Floyd et1</a></el-button>
+        <el-button ><a target="abc" href="http://logviewer-atl/proj/cip_floyd_genz/ip_regression/nbif.regression.main.floyd.nbif_et_2.group1.1/func_cov_report/dashboard.html">Floyd et2</a></el-button>
       </el-row>
     </el-header>
     <el-main>
