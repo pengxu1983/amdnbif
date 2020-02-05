621cb5ac28aaf2765c113c49d673df90e686b4a9
pengxu1983 <pengxu1983@163.com>
2020-02-05 12:44:08 +0800

    dbg

--- nbif3.0/web/src/components/SanityPage.vue
+++ nbif3.0/web/src/components/SanityPage.vue
@@ -1,40 +1,63 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+  <el-container>
+    <el-main>
+      <el-row>
+        <el-form :inline="true" :model="projectinfo" class="demo-form-inline">
+          <el-form-item label="Project">
+            <el-select v-model="projectinfo.projectname" placeholder="Project" @change="getprojects()">
+              <el-option 
+                v-for="oneproject in projects"
+                :label="oneproject.projectname" 
+                :value="oneproject.projectname"
+              >
+              </el-option>
+            </el-select>
+          </el-form-item>
+          <el-form-item label="Variant">
+            <el-select v-model="projectinfo.variantname" placeholder="Variant" @change="getgroups()">
+              <el-option 
+                v-for="onevariant in validvariants"
+                :label="onevariant" 
+                :value="onevariant"
+              >
+              </el-option>
+            </el-select>
+          </el-form-item>
+          <el-form-item label="isBAPU">
+            <el-select v-model="projectinfo.isBAPU" placeholder="BAPU" @change="getgroups()">
+              <el-option 
+                v-for="item in options"
+                :label="item" 
+                :value="item"
+              >
+              </el-option>
+            </el-select>
+          </el-form-item>
+        </el-form>
+      </el-row>
+    </el-main>
+  </el-container>
 </template>
+
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: 'SanityPage',
   props: {
     msg: String
+  },
+  data(){
+    return {
+      codelines : [
+        'nbif2_0'
+      ],
+      branch_names : [
+        'nbif_mi200_lsd',
+        'nbif2_0_main'
+      ],
+      selectedcodeline  : 'nbif2_0',
+      selectedbranch_name : 'nbif2_0_main',
+    }
   }
 }
 </script>
