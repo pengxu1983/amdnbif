5a84b052b8b0c664bdc10279c9f40a793e393b3b
Peng <benpeng@ltipc969.amd.com>
2020-03-18 05:10:59 -0400

    dbg

--- nbif5.0/srv/api/controllers/try/onecaseend.js
+++ nbif5.0/srv/api/controllers/try/onecaseend.js
@@ -8,7 +8,42 @@ module.exports = {
 
 
   inputs: {
-
+    testname  : {
+      type  : 'string'
+    },
+    codeline  : {
+      type  : 'string'
+    },
+    branch_name : {
+      type  : 'string'
+    },
+    changelist  : {
+      type  : 'string'
+    },
+    projectname   : {
+      type  : 'string'
+    },
+    variantname : {
+      type  : 'string'
+    },
+    shelve  : {
+      type  : 'string'
+    },
+    isBAPU  : {
+      type  : 'string'
+    },
+    seed  :{
+      type  : 'string'
+    },
+    kickoffdate : {
+      type  : 'string'
+    },
+    result  : {
+      type  : 'string'
+    },
+    signature : {
+      type  : 'string'
+    }
   },
 
 
@@ -17,8 +52,87 @@ module.exports = {
   },
 
 
-  fn: async function (inputs) {
-
+  fn: async function (inputs,exits) {
+    sails.log('/try/onecaseend');
+    sails.log(inputs);
+    let onecaseDB = await Regressiondetails.find({
+      testname    : inputs.testname,
+      projectname:  inputs.projectname,
+      variantname:  inputs.variantname,
+      kickoffdate:  inputs.kickoffdate,
+      codeline: inputs.codeline,
+      branch_name:  inputs.branch_name,
+      changelist: inputs.changelist,
+      isBAPU: inputs.isBAPU,
+      shelve: inputs.shelve
+    });
+    if(onecaseDB.length >1){
+      await Regressiondetails.destroy({
+        testname    : inputs.testname,
+        projectname:  inputs.projectname,
+        variantname:  inputs.variantname,
+        kickoffdate:  inputs.kickoffdate,
+        codeline: inputs.codeline,
+        branch_name:  inputs.branch_name,
+        changelist: inputs.changelist,
+        isBAPU: inputs.isBAPU,
+        shelve: inputs.shelve,
+      });
+      await Regressiondetails.create({
+        testname    : inputs.testname,
+        projectname:  inputs.projectname,
+        variantname:  inputs.variantname,
+        kickoffdate:  inputs.kickoffdate,
+        codeline: inputs.codeline,
+        branch_name:  inputs.branch_name,
+        changelist: inputs.changelist,
+        isBAPU: inputs.isBAPU,
+        shelve: inputs.shelve,
+        result: inputs.result,
+        seed  : inputs.seed,
+        signature : inputs.signature
+      });
+      return exits.success(JSON.stringify({
+        ok  : 'notok',
+        msg : 'duplicate case status found'
+      }));
+    }
+    if(onecaseDB.length ==  0){
+      await Regressiondetails.create({
+        testname    : inputs.testname,
+        projectname:  inputs.projectname,
+        variantname:  inputs.variantname,
+        kickoffdate:  inputs.kickoffdate,
+        codeline: inputs.codeline,
+        branch_name:  inputs.branch_name,
+        changelist: inputs.changelist,
+        isBAPU: inputs.isBAPU,
+        shelve: inputs.shelve,
+        result: inputs.result,
+        seed  : inputs.seed,
+        signature : inputs.signature
+      });
+      return exits.success(JSON.stringify({
+        ok  : 'notok',
+        msg : 'no case created'
+      }));
+    }
+    if(onecaseDB.length ==  1){
+      await Regressiondetails.update({
+        testname    : inputs.testname,
+        projectname:  inputs.projectname,
+        variantname:  inputs.variantname,
+        kickoffdate:  inputs.kickoffdate,
+        codeline: inputs.codeline,
+        branch_name:  inputs.branch_name,
+        changelist: inputs.changelist,
+        isBAPU: inputs.isBAPU,
+        shelve: inputs.shelve,
+      },{
+        result: inputs.result,
+        signature : inputs.signature
+      });
+    }
     // All done.
     return;
 
