e9790f8b810f7e81638d336afe30228c35ba131e
pengxu1983 <pengxu1983@163.com>
2020-04-29 00:36:48 -0400

    dbg

---
+++ nbif5.1/clt/clt_run_tasks/tools/RTlogin.js
@@ -0,0 +1,50 @@
+#!/home/benpeng/nbifweb_client/software/node/bin/node
+var mysql           = require('mysql');
+let querystring     = require('querystring');
+let http            = require('http');
+var moment          = require('moment');
+let process         = require('process');
+let cronJob         = require("cron").CronJob;
+let child_process   = require('child_process');
+let fs              = require('fs');
+let loginit         = function(){
+  return '[LOG]['+moment().format('YYYY-MM-DD HH:mm:ss')+'] ';
+};
+/////////////////////////
+//rt_login
+/////////////////////////
+let cron_rtlogin = new cronJob(rtlogintime.s+' '+rtlogintime.m+' * * * *',function(){
+  child_process.exec('~/nbifweb_client/software/tools/rtlogin',function(err,stdout,stderr){
+    if(err) {
+      throw err;
+    }
+    let regx  = /you must use a password/;
+    if(regx.test(stdout)){
+      let lines = stdout.split('\n');
+      for(let l=0;l<lines.length;l++){
+        console.log('[LOG]['+moment().format('YYYY-MM-DD HH:mm:ss')+']rt_login : '+lines[l]);
+      }
+      cron_check.start();
+    }
+    else{
+      //cron_check.stop();
+      child_process.execSync('rm -rf /home/benpeng/.jfrog/');
+      child_process.exec('~/nbifweb_client/software/tools/rtlogin',function(err1,result1,stderr1){
+        if(err1) {
+          child_process.execSync('mutt Benny.Peng@amd.com -s [NBIF][Sanity][RTLOGINFAIL] < /proj/cip_nbif_dv_3/amdnbif/nbif5.1/clt/clt_run_tasks/tools/RTlogin.fail');
+          throw err1;
+        }
+        if(regx.test(stdout)){
+          cron_check.start();
+        }
+        else{
+          child_process.execSync('mutt Benny.Peng@amd.com -s [NBIF][Sanity][RTLOGINFAIL] < /proj/cip_nbif_dv_3/amdnbif/nbif5.1/clt/clt_run_tasks/tools/RTlogin.fail');
+          throw 'need fix';
+        }
+      });
+    }
+  });
+},null,true,'Asia/Chongqing');
+/////////////////////////
+//=======================
+/////////////////////////
