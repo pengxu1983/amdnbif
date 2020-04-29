dd5fe6966b44486e938b3e25512947c0dc0ae8b2
pengxu1983 <pengxu1983@163.com>
2020-04-29 01:01:07 -0400

    dbg

--- nbif5.1/clt/clt_run_tasks/tools/RTlogin.js
+++ nbif5.1/clt/clt_run_tasks/tools/RTlogin.js
@@ -1,5 +1,4 @@
 #!/home/benpeng/nbifweb_client/software/node/bin/node
-var mysql           = require('mysql');
 let querystring     = require('querystring');
 let http            = require('http');
 var moment          = require('moment');
@@ -9,6 +8,11 @@ let child_process   = require('child_process');
 let fs              = require('fs');
 let loginit         = function(){
   return '[LOG]['+moment().format('YYYY-MM-DD HH:mm:ss')+'] ';
+};
+let rtloginstart    = moment().add(30,'seconds');
+let rtlogintime     = {
+  s : rtloginstart.format('ss'),
+  m : rtloginstart.format('mm'),
 };
 /////////////////////////
 //rt_login
@@ -24,10 +28,8 @@ let cron_rtlogin = new cronJob(rtlogintime.s+' '+rtlogintime.m+' * * * *',functi
       for(let l=0;l<lines.length;l++){
         console.log('[LOG]['+moment().format('YYYY-MM-DD HH:mm:ss')+']rt_login : '+lines[l]);
       }
-      cron_check.start();
     }
     else{
-      //cron_check.stop();
       child_process.execSync('rm -rf /home/benpeng/.jfrog/');
       child_process.exec('~/nbifweb_client/software/tools/rtlogin',function(err1,result1,stderr1){
         if(err1) {
@@ -35,7 +37,6 @@ let cron_rtlogin = new cronJob(rtlogintime.s+' '+rtlogintime.m+' * * * *',functi
           throw err1;
         }
         if(regx.test(stdout)){
-          cron_check.start();
         }
         else{
           child_process.execSync('mutt Benny.Peng@amd.com -s [NBIF][Sanity][RTLOGINFAIL] < /proj/cip_nbif_dv_3/amdnbif/nbif5.1/clt/clt_run_tasks/tools/RTlogin.fail');
