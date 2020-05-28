e4d6a772cdc25e62face2799f58a660632d8db53
pengxu1983 <pengxu1983@163.com>
2020-05-28 10:27:13 -0400

    dbg

---
+++ nbif6.0/clt6.0/sanity6.0/api/controllers/sync.js
@@ -0,0 +1,28 @@
+module.exports = {
+
+
+  friendlyName: 'Sync',
+
+
+  description: 'Sync something.',
+
+
+  inputs: {
+
+  },
+
+
+  exits: {
+
+  },
+
+
+  fn: async function (inputs) {
+
+    // All done.
+    return;
+
+  }
+
+
+};
