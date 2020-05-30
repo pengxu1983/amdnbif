09f9a473e887c1a57ccecab05c66eb4de6d8383c
pengxu1983 <pengxu1983@163.com>
2020-05-30 01:28:19 -0400

    dbg

---
+++ nbif6.0/clt6.0/sanity6.0/api/controllers/clientrun.js
@@ -0,0 +1,28 @@
+module.exports = {
+
+
+  friendlyName: 'Clientrun',
+
+
+  description: 'Clientrun something.',
+
+
+  inputs: {
+
+  },
+
+
+  exits: {
+
+  },
+
+
+  fn: async function (inputs) {
+
+    // All done.
+    return;
+
+  }
+
+
+};
