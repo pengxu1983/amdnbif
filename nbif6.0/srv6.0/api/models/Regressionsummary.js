2d2527de3eca9274a62bdca47d0516713639a083
pengxu1983 <pengxu1983@163.com>
2020-06-18 15:15:05 +0800

    dbg

---
+++ nbif6.0/srv6.0/api/models/Regressionsummary.js
@@ -0,0 +1,93 @@
+/**
+ * Regressionsummary.js
+ *
+ * @description :: A model definition represents a database table/collection.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+
+    //  ╔═╗╦═╗╦╔╦╗╦╔╦╗╦╦  ╦╔═╗╔═╗
+    //  ╠═╝╠╦╝║║║║║ ║ ║╚╗╔╝║╣ ╚═╗
+    //  ╩  ╩╚═╩╩ ╩╩ ╩ ╩ ╚╝ ╚═╝╚═╝
+    codeline    : {
+      type      : 'string'
+    },
+    branch_name : {
+      type      : 'string'
+    },
+    changelist  : {
+      type      : 'string'
+    },
+    shelve      : {
+      type      : 'string'
+    },
+    variantname : {
+      type      : 'string'
+    },
+    kickoffdate : {
+      type      : 'string'
+    },
+    describe    : {
+      type      : 'string'
+    },
+    grouplist   : {
+      type      : 'string'
+    },
+    testnumber  : {
+      type      : 'string'
+    },
+    passnumber  : {
+      type      : 'string'
+    },
+    passrate    : {
+      type      : 'string'
+    },
+    failnumber  : {
+      type      : 'string'
+    },
+    notrunnumber  : {
+      type      : 'string'
+    },
+    runningnumber : {
+      type      : 'string'
+    },
+    isOfficial  : {
+      type      : 'string'
+    },
+    isBAPU      : {
+      type      : 'string'
+    },
+    updatetime  : {
+      type      : 'string'
+    },
+    result      : {
+      type      : 'string'
+    },
+    treeRoot    : {
+      type      : 'string'
+    },
+    username    : {
+      type      : 'string'
+    },
+    bsubQlist   : {
+      type      : 'string',
+      columnType  : 'longtext'
+    }
+    
+
+    //  ╔═╗╔╦╗╔╗ ╔═╗╔╦╗╔═╗
+    //  ║╣ ║║║╠╩╗║╣  ║║╚═╗
+    //  ╚═╝╩ ╩╚═╝╚═╝═╩╝╚═╝
+
+
+    //  ╔═╗╔═╗╔═╗╔═╗╔═╗╦╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
+    //  ╠═╣╚═╗╚═╗║ ║║  ║╠═╣ ║ ║║ ║║║║╚═╗
+    //  ╩ ╩╚═╝╚═╝╚═╝╚═╝╩╩ ╩ ╩ ╩╚═╝╝╚╝╚═╝
+
+  },
+
+};
+
