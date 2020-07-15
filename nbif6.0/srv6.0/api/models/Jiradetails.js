2b2b6c64594ebf72c5dc4732ce4152b95e445c81
pengxu1983 <pengxu1983@163.com>
2020-07-14 22:14:37 -0400

    dbg

---
+++ nbif6.0/srv6.0/api/models/Jiradetails.js
@@ -0,0 +1,68 @@
+/**
+ * Jiradetails.js
+ *
+ * @description :: A model definition represents a database table/collection.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+
+    //  ╔═╗╦═╗╦╔╦╗╦╔╦╗╦╦  ╦╔═╗╔═╗
+    //  ╠═╝╠╦╝║║║║║ ║ ║╚╗╔╝║╣ ╚═╗
+    //  ╩  ╩╚═╩╩ ╩╩ ╩ ╩ ╚╝ ╚═╝╚═╝
+    reporter  : {
+      type    : 'string'
+    },
+    assignee  : {
+      type    : 'string'
+    },
+    createdDate : {
+      type    : 'string'
+    },
+    updatedDate : {
+      type    : 'string'
+    },
+    rejectedDate  : {
+      type    : 'string'
+    }
+    dueDate     : {
+      type    : 'string'
+    },
+    closedDate  : {
+      type    : 'string'
+    },
+    deferredDate  : {
+      type    : 'string'
+    },
+    lastComment : {
+      type    : 'string',
+      columnType  : 'longtext'
+    },
+    description : {
+      type    : 'string',
+      columnType  : 'longtext'
+    },
+    key         : {
+      type    : 'string'
+    },
+    summary     : {
+      type    : 'string',
+      columnType  : 'longtext'
+    },
+
+
+    //  ╔═╗╔╦╗╔╗ ╔═╗╔╦╗╔═╗
+    //  ║╣ ║║║╠╩╗║╣  ║║╚═╗
+    //  ╚═╝╩ ╩╚═╝╚═╝═╩╝╚═╝
+
+
+    //  ╔═╗╔═╗╔═╗╔═╗╔═╗╦╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
+    //  ╠═╣╚═╗╚═╗║ ║║  ║╠═╣ ║ ║║ ║║║║╚═╗
+    //  ╩ ╩╚═╝╚═╝╚═╝╚═╝╩╩ ╩ ╩ ╩╚═╝╝╚╝╚═╝
+
+  },
+
+};
+
